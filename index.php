ec68ff198510064939066541219dcc5580ee8e2c
dasha <lepskaadasha@gmail.com>
2021-01-14 14:34:47 +0300

    modified index.php

--- index.php
+++ index.php
@@ -55,6 +55,8 @@
         <input type="email" name="check" id="1">
         <input type="button" onclick="zad2()" id="btn_res2" value="Результат"><br>
         <label for="">Результат теста: <input type="text" readonly name="res2" id="res2"></label>
+        <input type="button" onclick="zad2()" id="btn_res2" value="Результат"><br>
+        <label for="">Результат теста: <input type="text" readonly name="res2" id="res2"></label>    
     </form>
 </body>
 </html>
